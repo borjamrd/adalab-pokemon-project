3a0016d0e629e1ce0c0841184114f5d28141ab9d
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2023-12-18 21:23:47 +0100

    updating MONGO_URI

--- adalab-server/configs/db.config.js
+++ adalab-server/configs/db.config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const connectToMongoDB = async () => {
     try {
+
         const connection = await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB:', connection.connection.host);
 
