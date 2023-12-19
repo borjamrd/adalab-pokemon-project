1e2ff33d53808d9c4a7bd1dfa6f23e4dfa03e27a
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2023-12-19 10:56:42 +0100

    updating next url with env variables

--- adalab-server/configs/config.js
+++ adalab-server/configs/config.js
@@ -10,7 +10,7 @@ dotenv.config({
 
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    BASE_URL: process.env.BASE_URL || 'http://localhost:4500',
+    BASE_URL: process.env.BASE_URL || 'http://localhost:4500/api',
     PORT: process.env.PORT || 4500,
     MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/pokemon'
 }
--- adalab-server/controllers/getAllPokemons.js
+++ adalab-server/controllers/getAllPokemons.js
@@ -1,15 +1,14 @@
 const basicPokemonSchema = require('../models/pokemon-model');
-
+const config = require('../configs/config');
 const getAllPokemons = async (req, res) => {
-
     try {
         const count = await basicPokemonSchema.countDocuments();
         const offset = req.query.offset || 0;
         const limit = req.query.limit || 20;
         const basicPokemonInfo = await basicPokemonSchema.find().limit(req.query.limit).skip(req.query.offset);
 
-        const nextUrl = offset < count && `${process.env.BASE_URL}/pokemon?offset=${Number(offset) + Number(limit)}&limit=${limit}`
-        const previousUrl = offset > 0 && `${process.env.BASE_URL}/pokemon?offset=${offset - limit}&limit=${limit}`
+        const nextUrl = offset < count && `${config.BASE_URL}/pokemon?offset=${Number(offset) + Number(limit)}&limit=${limit}`
+        const previousUrl = offset > 0 && `${config.BASE_URL}/pokemon?offset=${offset - limit}&limit=${limit}`
 
         res.status(200).json({
             count: count,
